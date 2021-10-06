f6d02bf62e7e8e0fd6f22da072e4f607a6edc72d
Tejada-Joyce <joyce_105_3@hotmail.com>
2021-10-06 06:50:46 -0600

    added option to remove items

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,15 +1,21 @@
-import { getLocalStorage } from "../js/utils.js";
+import {
+  getLocalStorage,
+  qs,
+  setClickforAll,
+  setLocalStorage,
+} from "../js/utils.js";
 
 function getCartContents() {
   let markup = "";
   const cartItems = getLocalStorage("so-cart");
   if (cartItems.length !== 0) {
     const htmlItems = cartItems.map((item) => renderCartItem(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    qs(".product-list").innerHTML = htmlItems.join("");
+    setClickforAll(".removeFromCart", removeFromCart);
   } else {
     const p = document.createElement("p");
-    const ul = document.querySelector(".product-list");
-    const section = document.querySelector(".products");
+    const ul = qs(".product-list");
+    const section = qs(".products");
     p.innerHTML = "There is nothing in your cart yet.";
     ul.style.display = "none";
     section.appendChild(p);
@@ -32,9 +38,22 @@ function renderCartItem(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <div class="cart-card__remove">
+    <button class="removeFromCart" data-id="${item.Id}">Remove from Cart</button>
+  </div>
 </li>`;
   console.log(newItem);
   return newItem;
+}
+
+//Function removes the first element that has the same id that was clicked
+function removeFromCart(el) {
+  const productId = el.getAttribute("data-id");
+  const cartItems = getLocalStorage("so-cart");
+  const index = cartItems.findIndex((product) => product.Id === productId);
+  cartItems.splice(index, 1);
+  setLocalStorage("so-cart", cartItems);
+  getCartContents();
 }
 
 getCartContents();
