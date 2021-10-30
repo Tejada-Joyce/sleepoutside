bb067e8d3c20aae8597bb76952f6387d5906b25b
Tejada-Joyce <joyce_105_3@hotmail.com>
2021-10-30 01:59:38 -0600

    added sort byy dropdown- missing the re-rendering

--- src/js/productList.js
+++ src/js/productList.js
@@ -14,26 +14,20 @@ export default class ProductList {
     // console.log(this.datasource);
     this.productList = await this.datasource.getProductsData(this.category);
 
-    // if (this.category === "tents") {
-    //   this.productList = await this.productList.filter(
-    //     (product) =>
-    //       product.Id == "880RR" ||
-    //       product.Id == "985RF" ||
-    //       product.Id == "985PR" ||
-    //       product.Id == "344YJ"
-    //   );
-    // }
-
     this.element = qs(this.element);
-    renderListWithTemplate(
-      this.template,
-      this.element,
-      this.productList,
-      this.prepareTemplate,
-      this.editBread
-    );
+    // renderListWithTemplate(
+    //   this.template,
+    //   this.element,
+    //   this.productList,
+    //   this.prepareTemplate,
+    //   this.editBread
+    // );
     this.editTitle();
     this.editBread();
+
+    qs("#sortTypes").addEventListener("change", (e) => {
+      this.sortListBy(e.target.value);
+    });
   }
 
   prepareTemplate(clone, product) {
@@ -66,5 +60,30 @@ export default class ProductList {
   editBread() {
     const bread = qs("section.breadcrumbs h3");
     bread.innerHTML += `${this.category} -> (${this.productList.length} Items)`;
+  }
+
+  sortListBy(type) {
+    if (type === "name") {
+      this.productList.sort((a, b) =>
+        a.NameWithoutBrand > b.NameWithoutBrand
+          ? 1
+          : b.NameWithoutBrand > a.NameWithoutBrand
+          ? -1
+          : 0
+      );
+      console.log(this.productList);
+    } else {
+      this.productList.sort((a, b) =>
+        a.ListPrice > b.ListPrice ? 1 : b.ListPrice > a.ListPrice ? -1 : 0
+      );
+      console.log(this.productList);
+    }
+    renderListWithTemplate(
+      this.template,
+      this.element,
+      this.productList,
+      this.prepareTemplate,
+      this.editBread
+    );
   }
 }
